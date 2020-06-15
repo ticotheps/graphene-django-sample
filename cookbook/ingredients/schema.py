76b38ab76da30b0d01b5728c1dad02ccd18fc118
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-15 15:57:28 -0400

    feat(schema): add new resolvers for 'category' and 'ingredient'

--- cookbook/ingredients/schema.py
+++ cookbook/ingredients/schema.py
@@ -11,11 +11,45 @@ class IngredientType(DjangoObjectType):
         model = Ingredient
         
 class Query(object):
+    category = graphene.Field(
+        CategoryType,
+        id=graphene.Int(),
+        name=graphene.String()
+    )
     all_categories = graphene.List(CategoryType)
     all_ingredients = graphene.List(IngredientType)
+    ingredient = graphene.Field(
+        IngredientType,
+        id=graphene.Int(),
+        name=graphene.String()
+    )
     
     def resolve_all_categories(self, info, **kwargs):
         return Category.objects.all()
     
     def resolve_all_ingredients(self, info, **kwargs):
         return Ingredient.objects.select_related('category').all()
+    
+    def resolve_category(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+        
+        if id is not None:
+            return Category.objects.get(pk=id)
+        
+        if name is not None:
+            return Category.objects.get(name=name)
+        
+        return None
+    
+    def resolve_ingredient(self, info, **kwargs):
+        id = kwargs.get('id')
+        name = kwargs.get('name')
+        
+        if id is not None:
+            return Ingredient.objects.get(pk=id)
+        
+        if name is not None:
+            return Ingredient.objects.get(name=name)
+        
+        return None
