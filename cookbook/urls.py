e91cd548eee46e577c5b8223aa0d10bd52eae83b
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-15 15:42:51 -0400

    feat(urls.py): add graphiql view to admin

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url, include
+from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^graphql$', GraphQLView.as_view(graphiql=True)),
 ]
