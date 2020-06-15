5b22f7e9719771cd987d111eac11eca2734ddfd6
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-15 15:34:49 -0400

    feat(schema): create a schema

---
+++ cookbook/ingredients/schema.py
@@ -0,0 +1,21 @@
+import graphene
+from graphene_django.types import DjangoObjectType
+from cookbook.ingredients.models import Category, Ingredient
+
+class CategoryType(DjangoObjetType):
+    class Meta:
+        model = Category
+        
+class IngredientType(DjangoObjectType):
+    class Meta:
+        model: Ingredient
+        
+class Query(object):
+    all_categories = graphene.List(CategoryType)
+    all_ingredients = graphene.List(IngredientType)
+    
+    def resolve_all_categories(self, info, **kwargs):
+        return Category.objects.all()
+    
+    def resolve_all_ingredients(self, info, **kwargs):
+        return Ingredient.objects.select_related('category').all()
