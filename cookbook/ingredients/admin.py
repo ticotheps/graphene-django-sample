106d9998b81804f57c9648d5ed076861fbf9e033
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-06-15 15:23:59 -0400

    feat: register models with admin site

--- cookbook/ingredients/admin.py
+++ cookbook/ingredients/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from cookbook.ingredients.models import Category, Ingredient
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Ingredient)
